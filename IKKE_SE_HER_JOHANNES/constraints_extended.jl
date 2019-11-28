af5d00f376cc12c97da5b7a07492aef0650404cb
Harald Aarskog <44364067+haraldaarskog@users.noreply.github.com>
2019-11-28 13:54:17 +0100

    Add files via upload

--- IKKE_SE_HER_JOHANNES/constraints_extended.jl
+++ IKKE_SE_HER_JOHANNES/constraints_extended.jl
@@ -21,37 +21,33 @@
 #@constraint(m,[d=1:D], a[d] <= sum(q[d,r]-p[d,r] for r=1:R))
 
 #minimum number of hours a radiologist can work per day
-@constraint(m,[d=1:D,r=1:R], t[d,r] + sum(T_im[i,m]*w[i,u,d,r,m]  for i=1:I,u=1:U, m in [1,2])+sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I)>=K_dr[d,r]-Q_d)
+@constraint(m,[d=1:D,r=1:R], t[d,r] + sum(T_im[i,m]*extended[i,u,h,d,r,m]  for i=1:I,u=1:U, h=1:H, m =1:M)+sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I)>=K_dr[d,r]-Q_d)
 
 #maximum number of hours a radiologist can work per day
-@constraint(m,[d=1:D,r=1:R], t[d,r] + sum(T_im[i,m]*w[i,u,d,r,m]  for i=1:I,u=1:U, m in [1,2])+sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I)<=K_dr[d,r]+Q_d)
+@constraint(m,[d=1:D,r=1:R], t[d,r] + sum(T_im[i,m]*extended[i,u,h,d,r,m]  for i=1:I,u=1:U, h=1:H, m=1:M)+sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I)<=K_dr[d,r]+Q_d)
 
 #The total number of working hours for each radiologist must equal Q_w
-@constraint(m, [r=1:R], sum(t[d,r] for d=1:D)+sum(T_im[i,m]*w[i,u,d,r,m] for i=1:I, u=1:U, d=1:D, m in [1,2]) + sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S, d=1:D)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I, d=1:D)==Q_w)
+@constraint(m, [r=1:R], sum(t[d,r] for d=1:D)+sum(T_im[i,m]*extended[i,u,d,h,r,m] for i=1:I, u=1:U, d=1:D, h=1:H, m in [1,2]) + sum(scanning_time_sd[s,d]*beta[s,d,r] for s=1:S, d=1:D)+sum(MDT_interpretation_time_i[i]*gamma[i,d,r] for i=1:I, d=1:D)==Q_w)
 
-#The sum of all internal interpretation tasks for u a given day h must equal the number of w[m=1] the same day
-#The same goes to v.
-@constraint(m,[i=1:I,u=1:U,h=1:H, r=1:R], sum(u_i[i,u,d,h,r] for d=1:D) == sum(w[i,u,h,r,m] for m=1))
-@constraint(m,[i=1:I,u=1:U,h=1:H, r=1:R], sum(v[i,u,d,h,r] for d=1:D) == sum(w[i,u,h,r,m] for m=2))
 
 #Making sure that internal interpretations can be done the current and the following week
 for i=1:I, u=1:U, d=1:D
     if d+B_iu[i,u] <= 7
-        @constraint(m, [i,u,d], sum(x[i,u,s,d] for s=1:S) - sum(u_i[i,u,d,h,r] for r=1:R,h=d:d+B_iu[i,u] if h <= D) == 0)
+        @constraint(m, [i,u,d], sum(x[i,u,s,d] for s=1:S) - sum(extended[i,u,d,h,r,m] for r=1:R,m=1,h=d:d+B_iu[i,u] if h <= D) == 0)
     else
-        @constraint(m, [i,u,d], sum(x[i,u,s,d] for s=1:S) - sum(u_i[i,u,d,h,r] for r=1:R,h=d:d+B_iu[i,u] if h <= D) - sum(u_i[i,u,d,h,r] for r=1:R, h=1:(d+B_iu[i,u])%7 if h<d) == 0)
+        @constraint(m, [i,u,d], sum(x[i,u,s,d] for s=1:S) - sum(extended[i,u,d,h,r,m] for r=1:R,m=1,h=d:d+B_iu[i,u] if h <= D) - sum(extended[i,u,d,h,r,m] for r=1:R,m=1, h=1:(d+B_iu[i,u])%7 if h<d) == 0)
     end
 end
 
 #Making sure that external interpretations can be done the current and the following week
 for i=1:I, u=1:U, d=1:D
     if d+B_iu[i,u] <= 7
-        @constraint(m, [i,u,d], sum(v[i,u,d,h,r] for r=1:R, h=d:d+B_iu[i,u] if h<=D) == E_iud[i,u,d])
-        @constraint(m, [i,u,d], sum(v[i,u,d,h,r] for r=1:R, h=1:d-1) == 0) #Setter starten av uka =0
-        @constraint(m, [i,u,d], sum(v[i,u,d,h,r] for r=1:R, h=(d+B_iu[i,u]+1:D) if h <= D)==0)  #20
+        @constraint(m, [i,u,d], sum(extended[i,u,d,h,r,m] for m=2, r=1:R, h=d:d+B_iu[i,u] if h<=D) == E_iud[i,u,d])
+        @constraint(m, [i,u,d], sum(extended[i,u,d,h,r,m] for m=2, r=1:R, h=1:d-1) == 0) #Setter starten av uka =0
+        @constraint(m, [i,u,d], sum(extended[i,u,d,h,r,m] for m=2, r=1:R, h=(d+B_iu[i,u]+1:D) if h <= D)==0)  #20
     else
-        @constraint(m, [i,u,d], sum(v[i,u,d,h,r] for r=1:R, h=d:d+B_iu[i,u] if h<=D) + sum(v[i,u,d,h,r] for r=1:R, h=1:(d+B_iu[i,u])%7 if h<d) == E_iud[i,u,d])
-        @constraint(m, [i,u,d], sum(v[i,u,d,h,r] for r=1:R,h=((d+B_iu[i,u])%7) + 1:(d-1)) == 0)    #setter midten av uka =0
+        @constraint(m, [i,u,d], sum(extended[i,u,d,h,r,m] for m=2, r=1:R, h=d:d+B_iu[i,u] if h<=D) + sum(extended[i,u,d,h,r,m] for m=2, r=1:R, h=1:(d+B_iu[i,u])%7 if h<d) == E_iud[i,u,d])
+        @constraint(m, [i,u,d], sum(extended[i,u,d,h,r,m] for m=2, r=1:R,h=((d+B_iu[i,u])%7) + 1:(d-1)) == 0)    #setter midten av uka =0
     end
 end
 
@@ -63,7 +59,7 @@ end
 
 #The number of internal interpretations must equal number of assigned slots
 #We have to include this constraint, otherwise the w can be set to meet G_dr
-@constraint(m, sum(w[i,u,d,r,m] for i=1:I, u=1:U, d=1:D, r=1:R, m=1)==sum(x[i,u,s,d] for i=1:I, u=1:U, s=1:S, d=1:D))
+@constraint(m, sum(extended[i,u,d,h,r,m] for h=1:H, i=1:I, u=1:U, d=1:D, r=1:R, m=1)==sum(x[i,u,s,d] for i=1:I, u=1:U, s=1:S, d=1:D))
 
 #Constraints that deals with demand. Either comment out the two first ones, or the third one.
 #@constraint(m, [i=1:I, u=1:U], sum(x[i,u,s,d] for s=1:S, d=1:D)<=(D_iuW_NY[i,u]+Percentage)*sum(x[i,u,s,d] for i=1:I, u=1:U, s=1:S, d=1:D))
@@ -89,10 +85,10 @@ end
 #EXTENSIONS - NOT INCLUDED IN THE INITIAL MODEL
 
 #Radiologist specializations
-@constraint(m, [i=1:I, r=1:R], sum(w[i,u,d,r,m] for u=1:U, d=1:D, m=1:M) <=1000*Preferanse_ir[i,r])#+ gamma[i,d,r]
+#@constraint(m, [i=1:I, r=1:R], sum(extended[i,u,d,h,r,m]+ gamma[i,d,r] for u=1:U, d=1:D, h=1:H, m=1:M) <=1000*Preferanse_ir[i,r])
 
 #@constraint(m, [r=1:R-1], sum(beta[s,d,r] for s=1:S, d=1:D)<=sum(beta[s,d,r+1] for s=1:S, d=1:D))
-#@constraint(m, [r=1:R-1], sum(gamma[i,d,r] for i=1:I, d=1:D)<=sum(gamma[i,d,r+1] for i=1:I, d=1:D))
+#@constraint(m, [r=1:R-1], sum(gamma[i,d,r] for i=1:I, d=1:D)>=sum(gamma[i,d,r+1] for i=1:I, d=1:D))
 
 
 #****************************************************************************************
--- IKKE_SE_HER_JOHANNES/main.jl
+++ IKKE_SE_HER_JOHANNES/main.jl
@@ -1,14 +1,14 @@
 using JuMP, Gurobi, PyCall
 
 #defining the input and output files
-input_file="/Users/johanlin/Downloads/Fordypningsprosjekt-master/IKKE_SE_HER_JOHANNES/BP_parameters.txt"
-output_file="/Users/johanlin/Downloads/Fordypningsprosjekt-master/IKKE_SE_HER_JOHANNES/output/output_TEST.txt"
+input_file="/Users/hara/Downloads/Fordypningsprosjekt-master/IKKE_SE_HER_JOHANNES/BP_parameters_OUS.txt"
+output_file="/Users/hara/Downloads/Fordypningsprosjekt-master/IKKE_SE_HER_JOHANNES/output/output_TEST.txt"
 
 #sets and parameters
 include("setsAndParameters.jl")
 
 #***********************************Variables***********************************
-m = Model(with_optimizer(Gurobi.Optimizer))
+m = Model(with_optimizer(Gurobi.Optimizer,TimeLimit=110))
 @variable(m, p[1:D,1:R] >= 0)
 @variable(m, q[1:D,1:R] >= 0)
 @variable(m, x[1:I,1:U,1:S,1:D] >= 0, Int)
@@ -21,6 +21,8 @@ m = Model(with_optimizer(Gurobi.Optimizer))
 @variable(m, t[1:D,1:R] >=0)
 @variable(m, y[1:D] >=0)
 @variable(m, a[1:D] >=0)
+
+#@variable(m, extended[1:I,1:U,1:D,1:H,1:R,1:M] >=0, Int)
 #****************************************************************************************
 
 
@@ -33,8 +35,9 @@ m = Model(with_optimizer(Gurobi.Optimizer))
 
 #Constraints
 include("constraints.jl")
-
+#include("constraints_extended.jl")
 #***********************************Solving*****************************************************
+println("Running model with: ","I=",I,", U=",U,", S=",S,", D=",D,", H=",H,", R=",R,", M=",M)
 optimize!(m)
 println("Status: ",termination_status(m))
 println("Primal status: ",primal_status(m))
